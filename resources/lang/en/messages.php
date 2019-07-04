01d9b7b0b4615933308a13737b31084380528dcc
Tim Joosten <Tjoosten@users.noreply.github.com>
2019-07-04 02:16:16 +0200

    v1.1.0 (#1)
    * Implement MarkdownHttpImageUrl rule
    * Implement DoesNotContainUrl route

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,4 +2,6 @@
 
 return [
     'matchUserPassword' => 'The given password is not your current password.',
+    'markdownHttpImageUrl' => 'The :attribute field contains at least one image with an HTTP link.',
+    'doesNotContainUrlRule' => 'The :attribute field cannot contain an url.',
 ];
--- resources/lang/nl/messages.php
+++ resources/lang/nl/messages.php
@@ -2,4 +2,6 @@
 
 return [
     'matchUserPassword' => 'Het gegeven wachtwoord is niet je huidig wachtwoord.',
+    'markdownHttpImageUrl' => 'Het :attribute veld bevat minstens één afbeelding met een HTTP-koppeling.',
+    'doesNotContainUrlRule' => 'Het :attribute veld kan geen url bevatten.',
 ];
